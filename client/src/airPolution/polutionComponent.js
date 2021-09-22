b3c62d7290d9ca0f63b457ed90c3f0e205e63dd4
Afda <afiq.danish306@gmail.com>
2021-09-22 12:40:27 +0800

    trying again

--- client/src/airPolution/polutionComponent.js
+++ client/src/airPolution/polutionComponent.js
@@ -2,7 +2,7 @@ import { useState, useEffect} from "react";
 
 
 function PolutionComponent({data}) {
-    const [component,setComponent] = useState({
+    const [pollution,setPollution] = useState({
         co:null,
         nh3:null,
         no:null,
@@ -16,9 +16,9 @@ function PolutionComponent({data}) {
     useEffect(() => {
         
         data.then((res=>{
-            // console.log(res)
+            console.log(res)
             const air = res.data.list[0].components
-            setComponent({
+            setPollution({
                 co:air.co,
                 nh3:air.nh3,
                 no:air.no,
@@ -30,18 +30,20 @@ function PolutionComponent({data}) {
             })
         }))
 
-    }, )
+    }, [data]);
+
+    // console.log(data)
 
     return (
         <div className='place polution'>
-            <div className="component polution"><p className="label polution">Carbon Monoxide:</p><p className="unit polution">{component.co} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Sulphur Dioxide:</p><p className="unit polution">{component.so2} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Nitrogen Monoxide:</p><p className="unit polution">{component.no} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Nitrogen Dioxide:</p><p className="unit polution">{component.no2} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Coarse Particulate:</p><p className="unit polution">{component.pm10} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Fine Particles:</p><p className="unit polution">{component.pm2_5} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Ozone:</p><p className="unit polution">{component.o3} μg/m3</p></div>
-            <div className="component polution"><p className="label polution">Ammonia:</p><p className="unit polution">{component.nh3} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Carbon Monoxide:</p><p className="unit polution">{pollution.co} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Sulphur Dioxide:</p><p className="unit polution">{pollution.so2} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Nitrogen Monoxide:</p><p className="unit polution">{pollution.no} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Nitrogen Dioxide:</p><p className="unit polution">{pollution.no2} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Coarse Particulate:</p><p className="unit polution">{pollution.pm10} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Fine Particles:</p><p className="unit polution">{pollution.pm2_5} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Ozone:</p><p className="unit polution">{pollution.o3} μg/m3</p></div>
+            <div className="component polution"><p className="label polution">Ammonia:</p><p className="unit polution">{pollution.nh3} μg/m3</p></div>
         </div>
     )
 }
